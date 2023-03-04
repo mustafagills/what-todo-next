e037b80456e21ffde3ee31c02940fcfddda70e26
Mustafa <e2310035@ceng.metu.edu.tr>
2023-03-04 21:20:21 +0300

    hide api key

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,8 +2,6 @@ const API_URL = "https://todo.crudful.com";
 
 const API_TIMEOUT = 10000;
 
-const CF_ACCESS_KEY = "b9df03c07989849205a27cc768a449a6bcebbbfd";
-
-// const CF_ACCESS_KEY = "6adc5b1171f488464a6bb8a54d42876890971c03";
+const CF_ACCESS_KEY = process.env.REACT_APP_CF_ACCESS_KEY;
 
 export { API_URL, API_TIMEOUT, CF_ACCESS_KEY };
